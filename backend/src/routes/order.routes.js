6628965a998dc75f728cfe6b74f698548672f6c7
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-09-05 16:26:31 +0300

    Update Order and can created form missing medicin

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createOrder, getOrders, generateOrderFromMissingMedicines} = require("../controllers/order.controller");
+const {createOrder, getOrders, generateOrderFromMissingMedicines, updateOrderStatus} = require("../controllers/order.controller");
 const {authMiddleware} = require("../middlewares/auth.middleware");
 const roleMiddleware = require("../middlewares/role.middleware");
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/",authMiddleware,roleMiddleware(["owner"]),createOrder);
 router.get("/",authMiddleware,getOrders);
 router.post("/generate-from-missing",authMiddleware,roleMiddleware(["owner"]),generateOrderFromMissingMedicines);
+router.patch("/:id/status",authMiddleware,roleMiddleware(["owner"]),updateOrderStatus);
 
 module.exports = router;
